ffadfece568407b3adf1370cae499c09320a964c
Swetanshu Pandey <swetanshupandey1722@gmail.com>
2024-01-06 16:48:13 +0530

    latest commit

--- stage_01.py
+++ stage_01.py
@@ -1,2 +1,2 @@
-with open("artifact.txt","w") as f:
+with open("artifact1.txt","w") as f:
     f.write("my name is swetanshu stage one is done/")
--- stage_02.py
+++ stage_02.py
@@ -1,4 +1,4 @@
-with open("artifact.txt","r") as f:
+with open("artifact1.txt","r") as f:
     text = f.read()
 
 print(text)
